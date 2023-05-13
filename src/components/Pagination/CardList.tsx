73b9b7aa7d0cbaf39fe6ad5d5ea28f7630cda18c
RizaAthaya <111055458+RizaAthaya@users.noreply.github.com>
2023-05-13 10:22:13 +0700

    again

--- src/components/Pagination/CardList.tsx
+++ src/components/Pagination/CardList.tsx
@@ -7,11 +7,14 @@ const CardList = (cardsBeasiswa: any) => {
     <div className="px-[1%] w-[85%] flex items-center justify-around">
       {cardsBeasiswa.map((items: any, index: number) => {
         <CardBeasiswa
-          key={index}
-          id={items.id}
-          tag={items.tag}
-          title={items.title}
-          date={items.date}
+        key={index}
+        tag_level={items.tag_level}
+        tag_country={items.tag_country}
+        tag_cost={items.tag_cost}
+        name={items.name}
+        open_registration={items.open_registration}
+        close_registration={items.close_registration}
+        provider={items.provider}
         />;
       })}
     </div>
